a7704c01b0755300d35ed4474cfc83dec260b6fc
Martina Kolářová <ruzickova1.martina@gmail.com>
2024-04-18 21:32:12 +0200

    Debugging,  upravené volání funkce

--- hra.js
+++ hra.js
@@ -12,31 +12,33 @@ const makeCircleOrCross = (evt) => {
     evt.target.classList.add('board__field--circle');
     document.querySelector('.player__turn').src = 'cross.svg';
   }
+  const gameBoard = Array.from(buttons).map((item) => {
+    if (item.classList.contains('board__field--circle')) {
+      return 'o';
+    } else if (item.classList.contains('board__field--cross')) {
+      return 'x';
+    } else {
+      return '_';
+    }
+  });
+  returnWinner(gameBoard);
 };
+
 const buttons = document.querySelectorAll('.game__board--field');
 buttons.forEach((button) => {
   button.addEventListener('click', makeCircleOrCross);
 });
 
-const gameBoard = Array.from(buttons).map((item) => {
-  if (item.classList.contains('board__field--circle')) {
-    return 'o';
-  } else if (item.classList.contains('board__field--cross')) {
-    return 'x';
-  } else {
-    return '_';
-  }
-});
-
-const returnWinner = () => {
+const returnWinner = (gameBoard) => {
+  console.log(gameBoard);
   const winner = findWinner(gameBoard);
+  console.log(winner);
   if (winner === 'o' || winner === 'x') {
-    alert(`Vyhrál hráč se symbolem "${winner()}"!`);
+    alert(`Vyhrál hráč se symbolem "${winner}"!`);
     location.reload();
   } else if (winner === 'tie') {
     alert('Hra skončila nerozhodně.');
   }
-  setTimeout(returnWinner, 500);
 };
 
 /*bonus confirm restart*/
